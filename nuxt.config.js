9cc0fc2a2ba4b01860885765a2caba69c60f4ee6
Emmanuel Nwogbo <emmanuelnwogbo@Emmanuels-MacBook-Pro.local>
2022-03-08 20:13:10 +0100

    check

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'the rebellion nft',
+    title: '  The Rebellion Nft',
     htmlAttrs: {
       lang: 'en'
     },
